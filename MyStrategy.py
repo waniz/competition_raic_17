d751020ddd322d6be011086d7e3f9767cf59b279
dmitrykustikov <waniz@mail.ru>
2017-11-07 14:25:16 +0300

    v0.1

--- MyStrategy.py
+++ MyStrategy.py
@@ -2,17 +2,42 @@ from model.ActionType import ActionType
 from model.Game import Game
 from model.Move import Move
 from model.Player import Player
+from model.PlayerContext import PlayerContext
+from model.TerrainType import TerrainType
+from model.Unit import Unit
+from model.Vehicle import Vehicle
+from model.VehicleType import VehicleType
+from model.VehicleUpdate import VehicleUpdate
+from model.WeatherType import WeatherType
 from model.World import World
 
 
 class MyStrategy:
-    def move(self, me: Player, world: World, game: Game, move: Move):
-        if world.tick_index == 0:
-            move.action = ActionType.CLEAR_AND_SELECT
-            move.right = world.width
-            move.bottom = world.height
+    random = None
+    terrain, weather = None, None
 
-        if world.tick_index == 1:
-            move.action = ActionType.MOVE
-            move.x = world.width / 2.0
-            move.y = world.height / 2.0
+    def game_init(self, world, game):
+        if not self.random:
+            self.random = game.random_seed
+        self.terrain = world.terrain_by_cell_x_y
+        self.weather = world.weather_by_cell_x_y
+
+    def tick_init(self, world, game, me):
+        pass
+
+    def move(self, me: Player, world: World, game: Game, move: Move):
+
+        if world.tick_index == 0:
+            self.game_init(world=world, game=game)
+        self.tick_init(world, game, me)
+
+        if me.remaining_action_cooldown_ticks > 0:
+            return
+
+
+
+
+
+
+
+
