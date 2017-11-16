296aac07ec1d031c13bcde48a70d3dde3ff990ee
dmitrykustikov <waniz@mail.ru>
2017-11-16 18:07:37 +0300

    v2

--- MyStrategy.py
+++ MyStrategy.py
@@ -1,5 +1,5 @@
-from collections import deque
 import copy
+from collections import deque
 
 from model.ActionType import ActionType
 from model.Game import Game
@@ -15,15 +15,16 @@ from model.WeatherType import WeatherType
 from model.World import World
 
 
-DEBUG = True
+DEBUG = False
 
 
 class MyStrategy:
     random = None
     terrain, weather = None, None
     STATE = None
-    regroup_orders = deque(maxlen=1000)
+    orders = deque(maxlen=1000)
     is_init_regroup = False
+    next_wait = 0
 
     def game_init(self, world, game, move, me):
         """ Init game, fill orders deque for regrouping """
@@ -39,64 +40,121 @@ class MyStrategy:
         new_vehicles = world.new_vehicles
 
         # get my units and formation in 2D-space
-        
+        my_vehicles = [x for x in new_vehicles if x.player_id == me.id]
+        mean_all_x = sum([my_vehicle.x for my_vehicle in my_vehicles]) / len(my_vehicles)
+        mean_all_y = sum([my_vehicle.x for my_vehicle in my_vehicles]) / len(my_vehicles)
 
+        if DEBUG:
+            print(mean_all_x, mean_all_y)
 
+        mover = copy.deepcopy(move)
+        mover.action = ActionType.CLEAR_AND_SELECT
+        mover.right = world.width
+        mover.bottom = world.height
+        self.orders.append(mover)
+
+        mover = copy.deepcopy(move)
+        mover.action = ActionType.SCALE
+        mover.factor = 1
+        mover.x = mean_all_x
+        mover.y = mean_all_y
+        self.orders.append(mover)
+
+        self.orders.append('wait 60')
+
+        mover = copy.deepcopy(move)
+        mover.action = ActionType.ROTATE
+        mover.angle = 1.5
+        mover.x = mean_all_x
+        mover.y = mean_all_y
+        self.orders.append(mover)
+
+        self.orders.append('wait 120')
+
+        mover = copy.deepcopy(move)
+        mover.action = ActionType.ROTATE
+        mover.angle = 1
+        mover.x = mean_all_x
+        mover.y = mean_all_y
+        self.orders.append(mover)
+
+        self.orders.append('wait 100')
+
+        mover = copy.deepcopy(move)
+        mover.action = ActionType.SCALE
+        mover.factor = 0.3
+        mover.x = mean_all_x
+        mover.y = mean_all_y
+        self.orders.append(mover)
+        self.orders.append('wait 200')
+
+        for _ in range(100):
+            mover = copy.deepcopy(move)
+            mover.action = ActionType.ROTATE
+            mover.angle = 1
+            mover.x = mean_all_x
+            mover.y = mean_all_y
+            self.orders.append(mover)
+            self.orders.append('wait 60')
+
+            mover = copy.deepcopy(move)
+            mover.action = ActionType.SCALE
+            mover.factor = 0.1
+            mover.x = mean_all_x
+            mover.y = mean_all_y
+            self.orders.append(mover)
+            self.orders.append('wait 60')
+
+            mover = copy.deepcopy(move)
+            mover.action = ActionType.CLEAR_AND_SELECT
+            mover.right = world.width
+            mover.bottom = world.height
+            self.orders.append(mover)
+            self.orders.append('wait 1')
+
+            mover = copy.deepcopy(move)
+            mover.action = ActionType.MOVE
+            mover.x = 600
+            mover.y = 600
+            mover.factor = 0.3
+            self.orders.append(mover)
+            self.orders.append('wait 20')
+        pass
 
     def tick_init(self, world, game, me):
         """ find out all my armies and types """
-        if len(self.regroup_orders) > 0:
-            return self.regroup_orders.popleft()
-        new_vehicles = world.vehicle_updates
-        print(len(new_vehicles))
+        if self.next_wait > 0:
+            self.next_wait -= 1
+            return None
+        if len(self.orders) > 0:
+            command = self.orders.popleft()
+            if type(command) is not str:
+                return command
+            else:
+                self.next_wait += int(command.split(' ')[1])
+        return None
 
     def move(self, me: Player, world: World, game: Game, move: Move):
         """ main method """
 
         if world.tick_index == 0:
             self.game_init(world=world, game=game, move=move, me=me)
-        temp_move = self.tick_init(world, game, me)
+        move_it = self.tick_init(world, game, me)
 
-        if world.tick_index < 2000:
-            self.STATE = 'init_regroup'
+        if move_it:
+            if DEBUG:
+                print('Receiver action', move_it.action)
+            move.action = move_it.action
+            move.right = move_it.right
+            move.bottom = move_it.bottom
+            move.x = move_it.x
+            move.y = move_it.y
+            move.factor = move_it.factor
+            move.max_speed = move_it.max_speed
+            move.angle = move_it.angle
 
-        if DEBUG:
-            print('[STATE] %s' % self.STATE)
-
-        if world.tick_index == 0:
-            move.action = ActionType.CLEAR_AND_SELECT
-            move.right = world.width
-            move.bottom = world.height
-        if world.tick_index == 1:
-            move.action = ActionType.MOVE
-            move.x = world.width / 2.0
-            move.y = world.height / 2.0
-
-        if me.remaining_action_cooldown_ticks > 0:
-            return
         else:
-            move = temp_move
-            print(move)
-
-    def _fill_commands_for_regroup(self, world, move):
-        """ fill regroup deque """
-
-        pass
-
-        # mover = copy.deepcopy(move)
-        # mover.action = ActionType.CLEAR_AND_SELECT
-        # mover.right = world.width
-        # mover.bottom = world.height
-        # self.regroup_orders.append(mover)
-        #
-        # mover = copy.deepcopy(move)
-        # mover.action = ActionType.MOVE
-        # mover.x = world.width / 2.0
-        # mover.y = world.height / 2.0
-        # self.regroup_orders.append(mover)
-
-
-
+            move = None
 
 
 
